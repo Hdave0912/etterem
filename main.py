0f1e4909f5984f67c732f0f4a752977e88f253e8
Hdave <hambergerdavid0912@gmail.com>
2021-12-10 17:58:38 +0100

    haha

--- main.py
+++ main.py
@@ -1,19 +1,23 @@
-try:
-    fileobject = open("kereset.txt","r")
-    tartalom = fileobject.readline()
-    print(tartalom)
-    fileobject.close()
-except FileNotFoundError:
-    print("nincs ilyen fajl")
-
 from enum import Enum
+class etelfajtak(Enum):
+    Csirke = 1
 
+fileobject = open("kereset.txt", "r")
+fileobject2 = open("csaketelfajtak.txt", "w")
+szoveg = fileobject.read()
+for kar in szoveg:
+    if kar in ('0', '1', '2', '3', '4', '5', '6', '7', '8', '9', " "):
+        fileobject2.write("")
+    else:
+        fileobject2.write(kar)
+fileobject.close()
+fileobject2.close()
 
-class napok(Enum):
-    Hetfo = 1
-    Kedd = 2
-    Szerda = 3
-    Csutortok = 4
-    Pentek = 5
-    Szombat = 6
-    Vasarnap = 7
+counter = 0
+fileobject2 = open("csaketelfajtak.txt", "r")
+data = fileobject2.read()
+for sor in fileobject2:
+    counter = sor.count('Csirke')
+print(counter)
+fileobject2.close()
+
